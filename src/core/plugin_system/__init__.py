987331f4835bbaa5b3e7c23a1b18e4ca1877b56c
senpaicito <140375853+senpaicito@users.noreply.github.com>
2025-10-24 01:25:16 -0700

    Init

--- src/core/plugin_system/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Lumi AI Plugin System
-"""
-from .plugin_manager import PluginManager
-from .plugin_base import BasePlugin
-from .plugin_registry import PluginRegistry
-from .events import PluginEvents
-
-__all__ = ['PluginManager', 'BasePlugin', 'PluginRegistry', 'PluginEvents']
